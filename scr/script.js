5367cf0e638e7c4b527fd73df059037be02b1053
Fanni Marosi <f.marosi@levigo.de>
2022-05-26 13:19:44 +0200

    Test marker grouping plugin

--- scr/script.js
+++ scr/script.js
@@ -4,18 +4,32 @@
 
 // config map
 let config = {
-    minZoom: 4,
-    maxZoom: 19,
-  };
+    minZoom: 4
+    //maxZoom: 25,
+};
 
-  // magnification with which the map will start
+// magnification with which the map will start
 const zoom = 11;
-// co-ordinates
+// coordinates
 const lat = 51.409486;
 const lng = -0.192261;
 
+// coordinate array with popup text
+const pointsA = [
+    [48.551338, 8.721353, "point A1"],
+    [48.572064, 9.503174, "point A2"],
+    [48.293244, 8.952484, "point A3"],
+    [48.185317, 9.571838, "point A4"],
+];
+
+const pointsB = [
+    [52.940363, 9.536133, "point B1"],
+    [52.663058, 10.06073, "point B2"],
+    [52.19919, 8.127136, "point B3"],
+];
+
 //  init map -> calling map 
-const map = L.map("map", config).setView([lat, lng], zoom);
+const map = L.map("map", config).setView([lat, lng], 8);
 
 //Adding default layer, used to load and display tile layers on the map
 // Most tile servers require attribution, which you can set under `Layer`
@@ -33,14 +47,11 @@ osm.addTo(map);
 var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     subdomains: 'abcd',
-    minZoom: 1,
-    maxZoom: 16,
     ext: 'jpg'
 });
-Stamen_Watercolor.addTo(map);
+//Stamen_Watercolor.addTo(map);
 
 var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
-    maxZoom: 18,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 //OpenStreetMap_DE.addTo(map);
@@ -81,3 +92,90 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
+// Extended `LayerGroup` that makes it easy
+// to do the same for all layers of its members
+const pA = new L.FeatureGroup();
+const pB = new L.FeatureGroup();
+const allMarkers = new L.FeatureGroup();
+
+// adding markers to the layer pointsA
+for (let i = 0; i < pointsA.length; i++) {
+    marker = L.marker([pointsA[i][0], pointsA[i][1]]).bindPopup(pointsA[i][2]);
+    pA.addLayer(marker);
+}
+
+// adding markers to the layer pointsB
+for (let i = 0; i < pointsB.length; i++) {
+    marker = L.marker([pointsB[i][0], pointsB[i][1]]).bindPopup(pointsB[i][2]);
+    pB.addLayer(marker);
+}
+
+// object with layers
+const overlayMaps = {
+    "point A": pA,
+    "point B": pB,
+};
+
+// centering a group of markers
+map.on("layeradd layerremove", function () {
+    // Create new empty bounds
+    let bounds = new L.LatLngBounds();
+    // Iterate the map's layers
+    map.eachLayer(function (layer) {
+        // Check if layer is a featuregroup
+        if (layer instanceof L.FeatureGroup) {
+            // Extend bounds with group's bounds
+            bounds.extend(layer.getBounds());
+        }
+    });
+
+    // Check if bounds are valid (could be empty)
+    if (bounds.isValid()) {
+        // Valid, fit bounds
+        map.flyToBounds(bounds);
+    } else {
+        // Invalid, fit world
+        // map.fitWorld();
+    }
+});
+
+L.Control.CustomButtons = L.Control.Layers.extend({
+    onAdd: function () {
+        this._initLayout();
+        this._addMarker();
+        this._removeMarker();
+        this._update();
+        return this._container;
+    },
+    _addMarker: function () {
+        this.createButton("add", "add-button");
+    },
+    _removeMarker: function () {
+        this.createButton("remove", "remove-button");
+    },
+    createButton: function (type, className) {
+        const elements = this._container.getElementsByClassName(
+            "leaflet-control-layers-list"
+        );
+        const button = L.DomUtil.create(
+            "button",
+            `btn-markers ${className}`,
+            elements[0]
+        );
+        button.textContent = `${type} markers`;
+
+        L.DomEvent.on(button, "click", function (e) {
+            const checkbox = document.querySelectorAll(
+                ".leaflet-control-layers-overlays input[type=checkbox]"
+            );
+
+            // Remove/add all layer from map when click on button
+            [].slice.call(checkbox).map((el) => {
+                el.checked = type === "add" ? false : true;
+                el.click();
+            });
+        });
+    },
+});
+
+new L.Control.CustomButtons(null, overlayMaps, { collapsed: false }).addTo(map);
