c9be69f3c86f66c14f18daf989de09e20958e068
Fanni Marosi <f.marosi@levigo.de>
2022-05-31 20:39:12 +0200

    Added GeoJson Layer Control
    overlayMaps, L.Control
    marker style

--- scr/script.js
+++ scr/script.js
@@ -11,8 +11,8 @@ let config = {
 // magnification with which the map will start
 const zoom = 11;
 // coordinates
-const lat = 51.409486;
-const lng = -0.192261;
+const lat = 48.474664;
+const lng = 8.942066;
 
 // coordinate array with popup text
 const pointsA = [
@@ -56,6 +56,13 @@ var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y
 });
 //OpenStreetMap_DE.addTo(map);
 
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+});
+//CartoDB_DarkMatter.addTo(map);
+
+/*
 //Marker test
 var marker = L.marker([51.5, -0.09]).addTo(map);
 marker.bindPopup("MArker").openPopup();
@@ -80,20 +87,22 @@ var popup_ = L.popup()
     .setLatLng([51.513, -0.09])
     .setContent("I am a standalone popup.")
     .openOn(map);
+*/
 
 var popup = L.popup();
-
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
-
 map.on('click', onMapClick);
+
+
 
 // Extended `LayerGroup` that makes it easy
 // to do the same for all layers of its members
+/*
 const pA = new L.FeatureGroup();
 const pB = new L.FeatureGroup();
 const allMarkers = new L.FeatureGroup();
@@ -108,13 +117,9 @@ for (let i = 0; i < pointsA.length; i++) {
 for (let i = 0; i < pointsB.length; i++) {
     marker = L.marker([pointsB[i][0], pointsB[i][1]]).bindPopup(pointsB[i][2]);
     pB.addLayer(marker);
+    
 }
-
-// object with layers
-const overlayMaps = {
-    "point A": pA,
-    "point B": pB,
-};
+*/
 
 // centering a group of markers
 map.on("layeradd layerremove", function () {
@@ -135,7 +140,8 @@ map.on("layeradd layerremove", function () {
         map.flyToBounds(bounds);
     } else {
         // Invalid, fit world
-        // map.fitWorld();
+        //map.fitWorld();
+        map.flyTo([lat, lng], 8);
     }
 });
 
@@ -177,5 +183,103 @@ L.Control.CustomButtons = L.Control.Layers.extend({
         });
     },
 });
+
+
+
+var dataGeoJson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Test 1",
+                "scan": "Rottenburg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.701171874999998,
+                    49.468124067331644
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Test 2",
+                "scan": "Rottenburg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    10.458984375,
+                    50.819818262156545
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Test 3",
+                "scan": "Dresden"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.1298828125,
+                    51.83577752045248
+                ]
+            }
+        }
+    ]
+}
+
+
+
+var Rottenburg = L.layerGroup().addTo(map);
+var Dresden = L.layerGroup().addTo(map);
+
+var Rottenburg = L.geoJson(dataGeoJson, {
+    pointToLayer: function (feature, latlng) {
+
+        return L.circleMarker(latlng, {
+            radius: 10,
+            opacity: .5,
+            color: "#ff7800",
+            fillColor: "#FFFFFF",
+            fillOpacity: 0.8
+
+        }).bindTooltip(feature.properties.name);
+    },
+
+    filter: function (feature, layer) {
+        return (feature.properties.scan === "Rottenburg");
+    }
+}).addTo(map)
+
+var Dresden = L.geoJson(dataGeoJson, {
+    pointToLayer: function (feature, latlng) {
+
+        return L.circleMarker(latlng, {
+            radius: 10,
+            opacity: .5,
+            color: "#FF19D9",
+            fillColor: "#FFFFFF",
+            fillOpacity: 0.2
+
+        }).bindTooltip(feature.properties.name);
+    },
+
+    filter: function (feature, layer) {
+        return (feature.properties.scan === "Dresden");
+    }
+}).addTo(map)
+
+
+// object with layers
+const overlayMaps = {
+    "Rottenburg": Rottenburg,
+    "Dresden": Dresden
+};
 
 new L.Control.CustomButtons(null, overlayMaps, { collapsed: false }).addTo(map);
